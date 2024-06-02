f8756d538481df685ef4ab84af7bc4160842df1e
Alfonso <contrerasalfonso.lino@gmail.com>
2024-06-01 23:24:18 -0500

    test

--- index.js
+++ index.js
@@ -30,6 +30,34 @@ app.use(cors(corsOptions));
 app.get('/',(req,res)=>{
   res.status(200).json('Hello')
 })
+
+app.get('/api/test/:id', function(req, res) {
+  // Lógica opcional para personalizar la respuesta OPTIONS
+  res.setHeader("Allow", "GET, POST");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).json({status:23});
+});
+
+
+app.head('/api/recurso', function(req, res) {
+  // Lógica opcional para obtener información sobre el recurso
+  const resourceInfo = {
+      nombre: "Recurso de ejemplo",
+      tipo: "Documento",
+      longitud: 1234,
+      ultimaModificacion: new Date().toISOString()
+  };
+
+  // Configuración de los encabezados de respuesta
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Length", JSON.stringify(resourceInfo).length);
+  res.setHeader("Last-Modified", resourceInfo.ultimaModificacion);
+
+  // Envío de la respuesta sin cuerpo
+  res.status(200).end();
+});
+
 app.use('/api/users',usersRouter);
 app.use('/api/sections',sectionsRouter);
 app.use('/api/subsections',subsectionsRouter);
